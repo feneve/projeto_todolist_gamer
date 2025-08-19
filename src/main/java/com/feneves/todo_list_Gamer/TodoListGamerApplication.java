8904effd02276133d61cb109244a16309e0247bb
FelipeNeves <felipewordmundo123@gmail.com>
2025-08-18 22:01:01 -0300

    Projeto criado

---
+++ src/main/java/com/feneves/todo_list_Gamer/TodoListGamerApplication.java
@@ -0,0 +1,13 @@
+package com.feneves.todo_list_Gamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListGamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feneves/todo_list_Gamer/TodoListGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feneves.todo_list_Gamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
